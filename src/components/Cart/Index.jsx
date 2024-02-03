a029e0dc8d620061bac463274059af7c6393cd2c
Dharmik79 <ddholariya199@gmail.com>
2024-02-03 07:54:37 -0600

    added the index for the cart value

--- src/components/Cart/Index.jsx
+++ src/components/Cart/Index.jsx
@@ -4,7 +4,11 @@ import { GlobalContext } from './../../context/States/GlobalState';
 const Index = () => {
   const { Global } = React.useContext(GlobalContext);
 
-  const total=Global?.cart?.reduce((acc, item) => acc + ((item?.price||100)*(item?.count ||1)), 0) ||0;
+  const total =
+    Global?.cart?.reduce(
+      (acc, item) => acc + (item?.price || 100) * (item?.count || 1),
+      0
+    ) || 0;
 
   return (
     <div class="page">
@@ -18,57 +22,60 @@ const Index = () => {
           <div class="cart__row">
             <div class="cart__col">
               <div class="cart__list">
-                {Global?.cart?.map((item, key) => {
-                  return (
-                    <div class="cart__item" key={key}>
-                      <a class="cart__preview" href="#">
-                        <img
-                          class="cart__pic"
-                          src={item?.image?.[0]}
-                          alt=""
-                        />
-                      </a>
-                      <div class="cart__details">
-                        <a class="cart__product" href="#">
-                          {item?.name}
+                {Global?.cart?.length > 0 &&
+                  Global?.cart?.map((item, key) => {
+                    return (
+                      <div class="cart__item" key={key}>
+                        <a class="cart__preview" href="#">
+                          <img
+                            class="cart__pic"
+                            src={item?.image?.[0]}
+                            alt=""
+                          />
                         </a>
-                        <div class="cart__price">
-                          <div class="cart__actual">${item?.price}</div>
-                        </div>
-                        <div class="cart__control">
-                          <div class="cart__counter counter js-counter">
-                            <button
-                              class="counter__btn counter__btn_minus js-counter-minus"
-                              type="button"
-                            >
-                              <svg class="icon icon-arrow-prev">
-                                <use xlink:href="img/sprite.svg#icon-arrow-prev"></use>
-                              </svg>
-                            </button>
-                            <input
-                              class="counter__input js-counter-input"
-                              type="text"
-                              value={item?.count ||1}
-                              size="3"
-                            />
-                            <button
-                              class="counter__btn counter__btn_plus js-counter-plus"
-                              type="button"
-                            >
-                              <svg class="icon icon-arrow-next">
-                                <use xlink:href="img/sprite.svg#icon-arrow-next"></use>
-                              </svg>
-                            </button>
+                        <div class="cart__details">
+                          <a class="cart__product" href="#">
+                            {item?.name}
+                          </a>
+                          <div class="cart__price">
+                            <div class="cart__actual">${item?.price}</div>
                           </div>
-                         
+                          <div class="cart__control">
+                            <div class="cart__counter counter js-counter">
+                              <button
+                                class="counter__btn counter__btn_minus js-counter-minus"
+                                type="button"
+                              >
+                                <svg class="icon icon-arrow-prev">
+                                  <use xlink:href="img/sprite.svg#icon-arrow-prev"></use>
+                                </svg>
+                              </button>
+                              <input
+                                class="counter__input js-counter-input"
+                                type="text"
+                                value={item?.count || 1}
+                                size="3"
+                              />
+                              <button
+                                class="counter__btn counter__btn_plus js-counter-plus"
+                                type="button"
+                              >
+                                <svg class="icon icon-arrow-next">
+                                  <use xlink:href="img/sprite.svg#icon-arrow-next"></use>
+                                </svg>
+                              </button>
+                            </div>
+                          </div>
                         </div>
                       </div>
-                    </div>
-                  );
-                })}
+                    );
+                  })}
+                {Global?.cart?.length == 0 && (
+                  <div class="cart__text">No Items Found</div>
+                )}
               </div>
             </div>
-            <div class="cart__col">
+          {Global?.cart?.length > 0 &&  <div class="cart__col">
               <div class="cart__receipt">
                 <div class="cart__category">Cart Total</div>
                 <div class="cart__wrap">
@@ -78,19 +85,21 @@ const Index = () => {
                   </div>
                   <div class="cart__line">
                     <div class="cart__text">Tax:</div>
-                    <div class="cart__text">${total*0.11}</div>
+                    <div class="cart__text">${total * 0.11}</div>
                   </div>
-                 
+
                   <div class="cart__line cart__line_total">
                     <div class="cart__text">Total:</div>
-                    <div class="cart__text">${Math.floor(total*1.11).toFixed(2)}</div>
+                    <div class="cart__text">
+                      ${Math.floor(total * 1.11).toFixed(2)}
+                    </div>
                   </div>
                 </div>
                 <a class="cart__btn btn btn_green btn_wide" href="/checkout">
                   Checkout
                 </a>
               </div>
-            </div>
+            </div>}
           </div>
         </div>
       </div>
